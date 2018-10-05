5bf84010a374e5260d2a357247e68cffefe61282
浅忆梦微凉 <3538922590@qq.com>
2018-10-05 09:25:06 +0800

    统计字符数行数与单词数

---
+++ 统计字符数单词数与行数/test.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<Windows.h>
+
+#define OUT 0
+#define IN 1
+
+int main()
+{
+	int state = OUT;
+	int nc = 0;//字符数
+	int nw = 0;//单词数
+	int n1 = 0;//行数
+	char c;
+	while ((c = getchar()) != EOF)
+	{
+		nc++;
+		if (c == '\n')
+		{
+			n1++;
+		}
+		else if (c == ' ' || c == '\t' || c == '\n')
+		{
+			state = OUT;
+		}
+		else if (state == OUT)
+		{
+			state = IN;
+			nw++;
+		}
+	}
+	printf("行数：%d\t单词数：%d\t字符数：%d\n", n1, nw, nc);
+	system("pause");
+	return 0;
+}
